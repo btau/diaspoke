251b9e0c5593ffd103597ad6f925c0cb6c4691ea
Brett Tau <brett.tau@gmail.com>
2017-08-05 20:27:11 -0500

    first commit

---
+++ Diaspoke/CustomColors.swift
@@ -0,0 +1,9 @@
+//
+//  CustomColors.swift
+//  Diaspoke
+//
+//  Created by Brett Tau on 8/4/17.
+//  Copyright © 2017 Diaspoke. All rights reserved.
+//
+
+import Foundation
---
+++ Diaspoke/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  Diaspoke
+//
+//  Created by Brett Tau on 8/4/17.
+//  Copyright © 2017 Diaspoke. All rights reserved.
+//
+
+import Foundation
